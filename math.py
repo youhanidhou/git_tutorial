702dd10ee1de79aca1d2f0a63253960518e349ce
youhanidhou <saleh.youhanidhou-mohamed-salem@ugb.edu.sn>
2023-01-14 17:21:42 +0100

    implemented the mult function

--- math.py
+++ math.py
@@ -1,6 +1,11 @@
 #I  addneed to implement this
+import re
+
+
 def add(x,y):
-  return x+y
+  if (type(x)==int and type(y)==int):
+    return x+y
+  return None
   
 #Bob needs to implement this
 def mult(x,y):
